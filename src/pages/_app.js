5be67741b6b012c0808c0a2bdd0df52662b9e4b1
aniketkapse <aniketkapse1810@gmail.com>
2025-11-10 15:50:46 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,11 +6,11 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Tushar Suryawanshi | Frontend Developer</title>
+        <title>Aniket Kapse | Data Engineer</title>
         <meta name="description"
           content="Tushar Suryawanshi is a frontend developer specializing in HTML, CSS, Javascript, React, Git, Bootstrap, Tailwind, Next.js, and Webflow. Explore her portfolio to see her latest projects and learn more about her skills and experience." />
         <meta name="author" content="Tushar Suryawanshi" />
-        
+
         {/* Open Graph */}
         <meta property="og:title" content="Tushar Suryawanshi's Portfolio" />
         <meta property="og:description" content="Tushar Suryawanshi - Frontend Developer. Passionate about creating exceptional websites that merge functionality with aesthetics. Specializing in frontend development. Crafting user-friendly and responsive solutions that enhance user experiences." />
